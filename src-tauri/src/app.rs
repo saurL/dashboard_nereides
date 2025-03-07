3eb00c9aab514344237be97f52086afcec6d2c14
Lucas Sauron <sauronlucas@gmail.com>
2025-03-07 14:27:11 +0100

    fix

--- src-tauri/src/app.rs
+++ src-tauri/src/app.rs
@@ -116,11 +116,11 @@ impl App {
                         Ok(frame) => {
                             let id = frame.id();
                             let data = frame.data();
-                            info!("Donnée reçue {} {:?}", id, data);
+                            info!("Donnée reçue {:?} {:?}", id, data);
                             // Traitez les données du frame ici et émettez des événements en conséquence
                             // Par exemple, vous pouvez convertir les données en une valeur et émettre un événement
                             let value = data[0] as f64; // Conversion simplifiée pour l'exemple
-                            app_handle.emit("can_data", value).unwrap();
+                            app_handle.emit(id, value).unwrap();
                         }
                         Err(e) => {
                             error!("Erreur lors de la lecture du frame CAN: {:?}", e);
