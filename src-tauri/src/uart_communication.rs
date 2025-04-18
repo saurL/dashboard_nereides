7a9015770cd207c371b15a4e0cc9dd50d25d13b9
Lucas <sauronlucas@gmail.com>
2025-04-18 17:19:56 +0200

    erase any null data from the buffer

--- src-tauri/src/uart_communication.rs
+++ src-tauri/src/uart_communication.rs
@@ -56,6 +56,7 @@ impl UartCommunication {
 
                 match port.read(&mut buffer) {
                     Ok(bytes_read) if bytes_read > 0 => {
+                        buffer.retain(|&x| x != 0);
                         total_buffer.extend(buffer);
                         while let Ok(Some((size, bytes_read))) = decode_varint(&total_buffer) {
                             if size as usize > total_buffer.len() - bytes_read {
