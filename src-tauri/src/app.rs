d640e931735769b892a16555061b3dcae50ed42f
Lucas Sauron <sauronlucas@gmail.com>
2025-04-24 15:45:14 +0200

    changed app location

--- src-tauri/src/app.rs
+++ src-tauri/src/app.rs
@@ -9,21 +9,23 @@ use tokio::sync::Mutex;
 use tokio::time::sleep;
 
 use crate::csv_writer::Csv_writter;
-use indexmap::IndexMap;
-
-use crate::mqtt::MQTT;
-use crate::uart_communication::{self, UartCommunication};
+#[cfg(target_os = "linux")]
 use crate::gps::Gps;
+use crate::mqtt::MQTT;
+use crate::uart_communication::UartData;
+use crate::uart_communication::{self, UartCommunication};
+use indexmap::IndexMap;
 use rand::rngs::StdRng;
 use rand::{Rng, SeedableRng};
 use serde_json::Value;
-use tokio::sync::mpsc::{channel, Receiver};
-use crate::uart_communication::UartData;
 use std::time::Instant;
+use tokio::sync::mpsc::{channel, Receiver};
 #[derive(Clone)]
 pub struct App {
     #[cfg(target_os = "linux")]
     uart_communication: Option<Arc<UartCommunication>>,
+    #[cfg(target_os = "linux")]
+    gps: Gps,
 
     #[cfg(not(target_os = "linux"))]
     uart_communication: Option<Arc<()>>, // Remplacer par un type générique ou un autre champ si nécessaire.
@@ -33,7 +35,6 @@ pub struct App {
     scv_writer: Csv_writter,
     data_api: IndexMap<&'static str, Option<f64>>,
     rx: Arc<Mutex<Receiver<UartData>>>,
-    gps: Gps,
     elapsed_time_data_sent: Instant,
 }
 
@@ -85,6 +86,7 @@ impl App {
         {
             let uart_communication = Some(UartCommunication::new("/dev/serial0", 1000000, tx));
         }
+        #[cfg(target_os = "linux")]
         let gps = Gps::new();
         let elapsed_time_data_sent = Instant::now();
         let instance: App = App {
@@ -95,6 +97,7 @@ impl App {
             scv_writer,
             data_api,
             rx: Arc::new(Mutex::new(rx)),
+            #[cfg(target_os = "linux")]
             gps,
             elapsed_time_data_sent,
         };
@@ -102,31 +105,36 @@ impl App {
         instance
     }
 
-    pub fn treat_data(&mut self, data_name:&str , value: f64) {
-        info!("Envoit de l'événement {} avec la valeur {}",data_name,value);
+    pub fn treat_data(&mut self, data_name: &str, value: f64) {
+        info!(
+            "Envoit de l'événement {} avec la valeur {}",
+            data_name, value
+        );
         self.app_handle.emit(data_name, value).unwrap();
         self.update_mesures(data_name, value);
         if self.all_mesures_complete() {
-          
             let elapsed_time = self.elapsed_time_data_sent.elapsed().as_secs();
-            info!("Temps écoulé depuis le dernier envoit de donnée : {} secondes", elapsed_time);
-            
+            info!(
+                "Temps écoulé depuis le dernier envoit de donnée : {} secondes",
+                elapsed_time
+            );
+
             let data: IndexMap<&str, Option<f64>> = self.data_api.clone();
             let mut filtered_data: IndexMap<&str, f64> = data
                 .iter()
                 .filter_map(|(&key, value)| value.map(|v| (key, v)))
                 .collect();
             self.mqtt.send_event(filtered_data.clone());
-             
-            if let Some(uart_comm) = &self.uart_communication {
-                let good_packets = uart_comm.get_good_packet_count();
-                let bad_packets = uart_comm.get_bad_packet_count();
-                let good_packet_percentage = uart_comm.get_good_packet_percentage();
-                filtered_data.insert("good_packets", good_packets as f64);
-                filtered_data.insert("bad_packets", bad_packets as f64);
-                filtered_data.insert("pourcentage_of_good_packet", good_packet_percentage);
-                
-
+            #[cfg(target_os = "linux")]
+            {
+                if let Some(uart_comm) = &self.uart_communication {
+                    let good_packets = uart_comm.get_good_packet_count();
+                    let bad_packets = uart_comm.get_bad_packet_count();
+                    let good_packet_percentage = uart_comm.get_good_packet_percentage();
+                    filtered_data.insert("good_packets", good_packets as f64);
+                    filtered_data.insert("bad_packets", bad_packets as f64);
+                    filtered_data.insert("pourcentage_of_good_packet", good_packet_percentage);
+                }
             }
             filtered_data.insert("elapsed_time", elapsed_time as f64);
             self.scv_writer.write_data(filtered_data.clone()).unwrap();
@@ -137,10 +145,8 @@ impl App {
         }
     }
 
-    pub fn treat_data_string(&mut self, data_name:&str , value: String) {
+    pub fn treat_data_string(&mut self, data_name: &str, value: String) {
         self.app_handle.emit(data_name, value).unwrap();
-
-
     }
     pub fn update_mesures(&mut self, data_name: &str, value: f64) {
         if let Some(data) = self.data_api.get_mut(data_name) {
@@ -189,19 +195,15 @@ impl App {
 
             while let Some(json_value) = rx.lock().await.recv().await {
                 info!("Received JSON: {:?}", json_value);
-                match json_value{
-
+                match json_value {
                     UartData::Number(json_value) => {
                         instance.treat_data(&json_value.data_name, json_value.value);
-                    
+                    }
+                    UartData::String(json_value) => {
+                        instance.treat_data_string(&json_value.data_name, json_value.value);
+                    }
                 }
-                UartData::String(json_value) => {
-                    instance.treat_data_string(&json_value.data_name, json_value.value);
-                }
-            }
             }
         });
-        
     }
-
 }
