2f0bf596219891b38888d78858e91f30906f4703
Lucas Sauron <sauronlucas@gmail.com>
2025-05-17 22:55:53 +0200

    on ne prends pas les 0 dans le buffer

--- src-tauri/src/uart_communication.rs
+++ src-tauri/src/uart_communication.rs
@@ -15,18 +15,18 @@ pub struct UartCommunication {
     bad_packets: Arc<Mutex<u64>>,
     percentage: Arc<Mutex<f64>>,
 }
-#[derive(Deserialize, Serialize,Debug)]
+#[derive(Deserialize, Serialize, Debug)]
 pub struct UartDataNumber {
     pub data_name: String,
     pub value: f64,
 }
-#[derive(Deserialize, Serialize,Debug)]
+#[derive(Deserialize, Serialize, Debug)]
 
 pub struct UartDataString {
     pub data_name: String,
     pub value: String,
 }
-#[derive(Deserialize,Debug)]
+#[derive(Deserialize, Debug)]
 #[serde(untagged)]
 pub enum UartData {
     Number(UartDataNumber),
@@ -53,7 +53,7 @@ impl UartCommunication {
 
     pub fn start_reading(&self) -> JoinHandle<()> {
         let instance = self.clone();
-        
+
         spawn(async move {
             let mut total_buffer = Vec::new();
             loop {
@@ -61,37 +61,38 @@ impl UartCommunication {
 
                 match instance.port.lock().await.read(&mut buffer) {
                     Ok(bytes_read) if bytes_read > 0 => {
+                        if buffer.iter().all(|&x| x == 0) {
+                            continue;
+                        }
                         // Retirer tous les 0 du buffer
-                        buffer= buffer[..bytes_read].into();
-                       
-                        
+                        buffer = buffer[..bytes_read].into();
+
                         total_buffer.extend(buffer);
 
                         info!("Total buffer state: {:?}", total_buffer);
 
-
                         // Si jamais le deuxème élément du buffer n'est pas '{', on fait en sorte de chercher le prochain
                         if total_buffer.len() > 1 && total_buffer[1] != b'{' {
-                            if let Some(pos) = total_buffer.iter().skip(1).position(|&x| x == b'{') {
+                            if let Some(pos) = total_buffer.iter().skip(1).position(|&x| x == b'{')
+                            {
                                 total_buffer.drain(..pos); // Supprime tout jusqu'au premier '{'
                                 info!("Buffer after removing invalid data: {:?}", total_buffer);
-                            }
-                            else {
+                            } else {
                                 error!("Invalid data format, no '{{' found");
                                 total_buffer.clear(); // ou `return` si tu veux quitter complètement
                                 continue;
                             }
                         }
-                            match std::str::from_utf8(&total_buffer) {
-                                Ok(s) => info!("Total buffer as string: {:?}", s),
-                                Err(e) => {}
-                            };
+                        match std::str::from_utf8(&total_buffer) {
+                            Ok(s) => info!("Total buffer as string: {:?}", s),
+                            Err(e) => {}
+                        };
                         while let Ok(Some((size, bytes_read))) = decode_varint(&total_buffer) {
                             if size as usize > total_buffer.len() - bytes_read {
                                 error!("Invalid data length: {}", size);
                                 break; // ou `return` si tu veux quitter complètement
                             }
-                            
+
                             let data = &total_buffer[bytes_read..bytes_read + size as usize];
 
                             let data_str = match std::str::from_utf8(data) {
@@ -100,8 +101,8 @@ impl UartCommunication {
                                     error!("Failed to convert data to string: {} Cette erreur n'est absolument pas normale , des données sont perdues et pour que la communication perdure \nNous allons tronquer le buffer jusqu'a obtenir le prochain début de message", e);
                                     //S'il y a des caratères non valide, on tronque le buffer jusqu'au prochain début de message
                                     // On cherche le prochain début de message
-                                  
-                                            total_buffer.clear();
+
+                                    total_buffer.clear();
                                     let mut bad_packed = instance.bad_packets.lock().await;
                                     *bad_packed += 1;
                                     continue;
@@ -123,7 +124,7 @@ impl UartCommunication {
                             info!("Received data: {:?}", json_value);
                             *instance.good_packets.lock().await += 1;
                             instance.calculate_percentage().await;
-                   
+
                             if instance.tx.send(json_value).await.is_err() {
                                 error!("Failed to send data to channel");
                             }
